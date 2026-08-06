cc4d5833c002bf2ad1032e59e5ed692cb50c5bd2
somarc <8710267+somarc@users.noreply.github.com>
2026-08-06 13:22:57 -0400

    fix: support DA-authored shared fragments

---
+++ blocks/note/note.js
@@ -0,0 +1,3 @@
+export default function init() {
+  // Note presentation is CSS-only; the export satisfies the Docket block loader.
+}
--- scripts/lazy.js
+++ scripts/lazy.js
@@ -1,6 +1,5 @@
 (function loadLazy() {
   import('./utils/lazyhash.js');
-  import('./utils/favicon.js');
   import('./utils/footer.js').then(({ default: footer }) => footer());
   import('../deps/rum.js').then(({ sampleRUM }) => {
     sampleRUM('load');
